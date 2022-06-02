558604f6c324dffb25dd0c92fc74a768e4efc09b
dominhkha <kha.minhdo199@gmail.com>
2022-06-02 18:00:13 +0700

    test

--- GameTest.cpp
+++ GameTest.cpp
@@ -136,10 +136,10 @@ bool verifyAddCherry(){
 
   // if no cherry is added, return false
   if(actualCherryPos == nullptr){
-    return false
+    return false;
   }
 
-  return *setCherryPos.x == actualCherryPos.x && *setCherryPos.y == actualCherryPos.y;
+  return setCherryPos->x == actualCherryPos.x && &setCherryPos->y == actualCherryPos.y;
 }
 
 class Test: public CPPUNIT_NS::TestCase {
