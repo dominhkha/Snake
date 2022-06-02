a37738d66c56e4e04cb078230aced3a3934967ab
dominhkha <kha.minhdo199@gmail.com>
2022-06-02 10:34:13 +0700

    init commit

---
+++ SDL_utils.h
@@ -0,0 +1,28 @@
+#ifndef SDL_UTILS__H
+#define SDL_UTILS__H
+
+#include <iostream>
+#include <SDL.h>
+
+#include "Game.h"
+#include "constants.h"
+#include "Gallery.h"
+
+void logSDLError(std::ostream& os, 
+                 const std::string &msg, bool fatal = false);
+
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer,
+	int screenWidth, int screenHeight, const char* windowTitle) ;
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void waitUntilKeyPressed();
+
+// moved from main.cpp
+void renderSplashScreen();
+void renderGamePlay(SDL_Renderer*, const Game& game, Gallery* gallery);
+void renderGameOver(SDL_Renderer* renderer, const Game& game);
+void interpretEvent(SDL_Event e, Game& game);
+void updateRankingTable(const Game& game);
+
+#endif
---
+++ constants.h
@@ -0,0 +1,19 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#include <SDL.h>
+
+const int SCREEN_WIDTH = 900;
+const int SCREEN_HEIGHT = 600;
+const char WINDOW_TITLE[] = "Snake Game";
+
+const int BOARD_WIDTH = 30;
+const int BOARD_HEIGHT = 20;
+const int CELL_SIZE = 30;
+
+const SDL_Color BOARD_COLOR = {0, 0, 0};
+const SDL_Color LINE_COLOR = {128, 128, 128};
+
+const double STEP_DELAY = 0.2;
+
+#endif 
